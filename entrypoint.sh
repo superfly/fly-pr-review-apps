3877b971943ca500781f9533070f2ff1faf2bd36
Brent Dillingham <brentdillingham@gmail.com>
2022-01-27 23:52:23 -0500

    WIP

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,25 +1,37 @@
 #!/bin/sh -l
 
+set -ex
+
 if [ -n "$INPUT_PATH" ]; then
-  PREV_PATH=$(pwd)
   # Allow user to change directories in which to run Fly commands
   cd "$INPUT_PATH" || exit
 fi
 
 PR_NUMBER=$(echo "$GITHUB_REF" | awk 'BEGIN { FS = "/" } ; { print $3 }')
+EVENT_TYPE=$(jq -r .action /github/workflow/event.json)
 
-echo "PR_NUMBER: $PR_NUMBER"
-echo "app: $INPUT_NAME-pr$PR_NUMBER"
+app="$INPUT_NAME-pr$PR_NUMBER"
+region="${INPUT_REGION:-${FLY_REGION:-iad}}"
+org="${INPUT_ORG:-${FLY_ORG:-personal}}"
 
-env
+# PR was opened or reopened
+if [ "$EVENT_TYPE" = "opened" ] || [ "$EVENT_TYPE" = "reopened" ]; then
 
-cat /github/workflow/event.json
+  flyctl launch --now --name "$app" --region "$region" --org "$org" --copy-config
 
-ACTUAL_EXIT="$?"
+  # Attach postgres cluster to the new app if needed
+  if [ -n "$INPUT_POSTGRES" ]; then
+    flyctl postgres attach --postgres-app "$INPUT_POSTGRES"
+  fi
 
-if [ -n "$PREV_PATH" ]; then
-  # If we changed directories before, we should go back to where we were.
-  cd "$PREV_PATH" || exit
+# New commits were added to the PR, and this is an app we want to deploy
+elif [ "$EVENT_TYPE" = "synchronize" ] && [ "$INPUT_UPDATE" != "false" ]; then
+
+  flyctl deploy --app "$app"
+
+# PR was closed
+elif [ "$EVENT_TYPE" = "closed" ]; then
+
+  flyctl apps destroy "$app" -y
+
 fi
-
-exit $ACTUAL_EXIT
