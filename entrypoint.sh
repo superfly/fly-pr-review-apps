416f7ae9e10b9afd523ea2f2acb2d3b7a44df7aa
Nick Randall <nicksrandall@gmail.com>
2022-04-14 17:10:43 -0600

    push secrets on update as well

--- entrypoint.sh
+++ entrypoint.sh
@@ -38,10 +38,13 @@ fi
 if ! flyctl status --app "$app"; then
   flyctl launch --no-deploy --copy-config --name "$app" --image "$image" --region "$region" --org "$org"
   if [ -n "$INPUT_SECRETS" ]; then
-    echo $INPUT_SECRETS | tr " " "\n" | flyctl secrets import --app "$app" --verbose
+    echo $INPUT_SECRETS | tr " " "\n" | flyctl secrets import --app "$app"
   fi
   flyctl deploy --app "$app" --region "$region" --image "$image" --region "$region" --strategy immediate
 elif [ "$INPUT_UPDATE" != "false" ]; then
+  if [ -n "$INPUT_SECRETS" ]; then
+    echo $INPUT_SECRETS | tr " " "\n" | flyctl secrets import --app "$app" --detach
+  fi
   flyctl deploy --app "$app" --region "$region" --image "$image" --region "$region" --strategy immediate
 fi
 
