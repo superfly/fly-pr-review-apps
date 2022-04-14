095a0906c28f1cac73c2a557e7cad9e9dc454b3a
Nick Randall <nicksrandall@gmail.com>
2022-04-14 10:30:05 -0600

    adding support for adding secets to deployment

--- entrypoint.sh
+++ entrypoint.sh
@@ -37,8 +37,14 @@ fi
 # Deploy the Fly app, creating it first if needed.
 if ! flyctl status --app "$app"; then
   flyctl launch --now --copy-config --name "$app" --image "$image" --region "$region" --org "$org"
+  if [ -n "$INPUT_SECRETS" ]; then
+    echo $INPUT_SECRETS | flyctl secrets import --app "$app" --detach
+  fi
 elif [ "$INPUT_UPDATE" != "false" ]; then
   flyctl deploy --app "$app" --region "$region" --image "$image" --region "$region" --strategy immediate
+  if [ -n "$INPUT_SECRETS" ]; then
+    echo $INPUT_SECRETS | flyctl secrets import --app "$app" --detach
+  fi
 fi
 
 # Attach postgres cluster to the app if specified.
