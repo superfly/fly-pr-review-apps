b09edf9823867c3ca357ee0fa1a5122f2f5e658a
Vincent Billey <vincent.billey@gmail.com>
2022-05-05 15:38:19 +0200

    Create a DB every time

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,48 +13,53 @@ if [ -z "$PR_NUMBER" ]; then
   exit 1
 fi
 
-REPO_OWNER=$(jq -r .event.base.repo.owner /github/workflow/event.json)
-REPO_NAME=$(jq -r .event.base.repo.name /github/workflow/event.json)
+REPO_OWNER=$(jq -r .organization.login /github/workflow/event.json)
+REPO_NAME=$(jq -r .repository.name /github/workflow/event.json)
 EVENT_TYPE=$(jq -r .action /github/workflow/event.json)
 
-cat /github/workflow/event.json
+# Default the Fly app name to pr-{number}-{repo_owner}-{repo_name}
+app="${INPUT_NAME:-pr-$PR_NUMBER-$REPO_OWNER-$REPO_NAME}"
+postgres_app="${INPUT_POSTGRES_NAME:-pr-$PR_NUMBER-$REPO_OWNER-$REPO_NAME-postgres}"
+region="${INPUT_REGION:-${FLY_REGION:-cdg}}"
+org="${INPUT_ORG:-${FLY_ORG:-personal}}"
+image="$INPUT_IMAGE"
 
-# # Default the Fly app name to pr-{number}-{repo_owner}-{repo_name}
-# app="${INPUT_NAME:-pr-$PR_NUMBER-$REPO_OWNER-$REPO_NAME}"
-# region="${INPUT_REGION:-${FLY_REGION:-iad}}"
-# org="${INPUT_ORG:-${FLY_ORG:-personal}}"
-# image="$INPUT_IMAGE"
+if ! echo "$app" | grep "$PR_NUMBER"; then
+  echo "For safety, this action requires the app's name to contain the PR number."
+  exit 1
+fi
 
-# if ! echo "$app" | grep "$PR_NUMBER"; then
-#   echo "For safety, this action requires the app's name to contain the PR number."
-#   exit 1
-# fi
+# PR was closed - remove the Fly app if one exists and exit.
+if [ "$EVENT_TYPE" = "closed" ]; then
+  flyctl apps destroy "$app" -y || true
+  if [ -n "$INPUT_POSTGRES" ]; then
+    flyctl apps destroy "$postgres_app" -y || true
+  fi
+  exit 0
+fi
 
-# # PR was closed - remove the Fly app if one exists and exit.
-# if [ "$EVENT_TYPE" = "closed" ]; then
-#   flyctl apps destroy "$app" -y || true
-#   exit 0
-# fi
+# Deploy the Fly app, creating it first if needed.
+if ! flyctl status --app "$app"; then
+  flyctl create --name "$app" --org "$org"
+fi
 
-# # Deploy the Fly app, creating it first if needed.
-# if ! flyctl status --app "$app"; then
-#   flyctl create --name "$app" --org "$org"
-# fi
+# Attach postgres cluster to the app if specified.
+if [ -n "$INPUT_POSTGRES" ]; then
+  if ! flyctl status --app "$postgres_app"; then
+    flyctl postgres create --name "$postgres_app" --region "$region" --organization "$org" --vm-size shared-cpu-1x --volume-size 1 --initial-cluster-size 1 || true
+    flyctl postgres attach --app "$app" --postgres-app "$postgres_app" || true
+  fi
+fi
 
-# # Attach postgres cluster to the app if specified.
-# if [ -n "$INPUT_POSTGRES" ]; then
-#   flyctl postgres attach --app "$app" --postgres-app "$INPUT_POSTGRES" || true
-# fi
-
-# if [ "$INPUT_UPDATE" != "false" ]; then
-#   flyctl deploy --app "$app" --region "$region" --image "$image" --region "$region" --strategy immediate
-# fi
+if [ "$INPUT_UPDATE" != "false" ]; then
+  flyctl deploy --app "$app" --region "$region" --image "$image" --region "$region" --strategy immediate
+fi
 
 
-# # Make some info available to the GitHub workflow.
-# fly status --app "$app" --json >status.json
-# hostname=$(jq -r .Hostname status.json)
-# appid=$(jq -r .ID status.json)
-# echo "::set-output name=hostname::$hostname"
-# echo "::set-output name=url::https://$hostname"
-# echo "::set-output name=id::$appid"
+# Make some info available to the GitHub workflow.
+fly status --app "$app" --json >status.json
+hostname=$(jq -r .Hostname status.json)
+appid=$(jq -r .ID status.json)
+echo "::set-output name=hostname::$hostname"
+echo "::set-output name=url::https://$hostname"
+echo "::set-output name=id::$appid"
