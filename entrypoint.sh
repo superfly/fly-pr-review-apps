6026a3bbb653f678dcd1330f87b13519ed294486
jakubjurczak <jakub.jurczak@wayfdigital.com>
2022-10-24 11:04:30 +0200

    fix: bad deploy at launch

--- entrypoint.sh
+++ entrypoint.sh
@@ -43,7 +43,7 @@ if ! flyctl status --app "$app"; then
   if [ -n "$INPUT_SECRETS" ]; then
     echo $INPUT_SECRETS | tr " " "\n" | flyctl secrets import --app "$app"
   fi
-  flyctl deploy --app "$app" --region "$region" --image "$image" --region "$region" --strategy immediate
+  flyctl deploy --config "$config" --dockerfile "$dockerfile" --app "$app" --region "$region" --image "$image" --strategy immediate
 elif [ "$INPUT_UPDATE" != "false" ]; then
   flyctl deploy --config "$config" --dockerfile "$dockerfile" --app "$app" --region "$region" --image "$image" --strategy immediate
 fi
