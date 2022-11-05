e0d68ecbe53fc862aa66020a35ad94b6f8716704
Matthew Tovbin <tovbinm@gmail.com>
2022-11-05 10:22:06 -0700

    zz

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,8 @@ if [ -n "$INPUT_PATH" ]; then
   cd "$INPUT_PATH" || exit
 fi
 
+echo "Got workflow event: $(cat /github/workflow/event.json)"
+
 PR_NUMBER=$(jq -r .number /github/workflow/event.json)
 if [ -z "$PR_NUMBER" ]; then
   echo "This action only supports pull_request actions."
