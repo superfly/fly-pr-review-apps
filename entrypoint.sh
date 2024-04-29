342e49a1d68e2b994fe126a0d4b2753a5a9164f2
som-matrix <swaroopsatya36@gmail.com>
2024-04-29 17:19:29 +0530

    reset main required for upstream

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/sh -l
 
 set -ex
 
@@ -13,61 +13,58 @@ if [ -z "$PR_NUMBER" ]; then
   exit 1
 fi
 
-# Check the flyctl version
-flyctl version
-
-REPO_NAME=$(jq -r .repository.name /github/workflow/event.json)
+GITHUB_REPOSITORY_NAME=${GITHUB_REPOSITORY#$GITHUB_REPOSITORY_OWNER/}
 EVENT_TYPE=$(jq -r .action /github/workflow/event.json)
 
-# Default the Fly app name to {repo_name}-pr-{pr_number}
-app="${INPUT_NAME:-$REPO_NAME-pr-$PR_NUMBER}"
-# # Default the Fly app name to {repo_name}-pr-{pr_number}-postgres
-postgres_app="${INPUT_POSTGRES:-$REPO_NAME-pr-$PR_NUMBER-postgres}"
-region="${INPUT_REGION:-${FLY_REGION:-ord}}"
+# Default the Fly app name to pr-{number}-{repo_owner}-{repo_name}
+app="${INPUT_NAME:-pr-$PR_NUMBER-$GITHUB_REPOSITORY_OWNER-$GITHUB_REPOSITORY_NAME}"
+# Change underscores to hyphens.
+app="${app//_/-}"
+region="${INPUT_REGION:-${FLY_REGION:-iad}}"
 org="${INPUT_ORG:-${FLY_ORG:-personal}}"
-dockerfile="$INPUT_DOCKERFILE"
-
-# only wait for the deploy to complete if the user has requested the wait option
-# otherwise detach so the GitHub action doesn't run as long
-if [ "$INPUT_WAIT" = "true" ]; then
-  detach=""
-else
-  detach="--detach"
-fi
+image="$INPUT_IMAGE"
+config="${INPUT_CONFIG:-fly.toml}"
 
 if ! echo "$app" | grep "$PR_NUMBER"; then
   echo "For safety, this action requires the app's name to contain the PR number."
   exit 1
 fi
 
-# If PR is closed or merged, the app will be deleted
+# PR was closed - remove the Fly app if one exists and exit.
 if [ "$EVENT_TYPE" = "closed" ]; then
   flyctl apps destroy "$app" -y || true
-
-  message="Review app deleted."
-  echo "message=$message" >> $GITHUB_OUTPUT
   exit 0
 fi
 
 # Deploy the Fly app, creating it first if needed.
 if ! flyctl status --app "$app"; then
-  flyctl launch --no-deploy --copy-config --name "$app" --dockerfile "$dockerfile" --region "$region" --org "$org"
+  # Backup the original config file since 'flyctl launch' messes up the [build.args] section
+  cp "$config" "$config.bak"
+  flyctl launch --no-deploy --copy-config --name "$app" --image "$image" --region "$region" --org "$org"
+  # Restore the original config file
+  cp "$config.bak" "$config"
+fi
+if [ -n "$INPUT_SECRETS" ]; then
+  echo $INPUT_SECRETS | tr " " "\n" | flyctl secrets import --app "$app"
+fi
 
-  # Attach postgres cluster and set the DATABASE_URL
-  flyctl postgres attach "$postgres_app" --app "$app"
-  flyctl deploy $detach --app "$app" --region "$region" --strategy immediate --remote-only
+# Attach postgres cluster to the app if specified.
+if [ -n "$INPUT_POSTGRES" ]; then
+  flyctl postgres attach "$INPUT_POSTGRES" --app "$app" || true
+fi
 
-  statusmessage="Review app created. It may take a few minutes for the app to deploy."
-elif [ "$EVENT_TYPE" = "synchronize" ]; then
-  flyctl deploy $detach --app "$app" --region "$region" --strategy immediate --remote-only
-  statusmessage="Review app updated. It may take a few minutes for your changes to be deployed."
+# Trigger the deploy of the new version.
+echo "Contents of config $config file: " && cat "$config"
+if [ -n "$INPUT_VM" ]; then
+  flyctl deploy --config "$config" --app "$app" --region "$region" --image "$image" --strategy immediate --ha=$INPUT_HA --vm-size "$INPUT_VMSIZE"
+else
+  flyctl deploy --config "$config" --app "$app" --region "$region" --image "$image" --strategy immediate --ha=$INPUT_HA --vm-cpu-kind "$INPUT_CPUKIND" --vm-cpus $INPUT_CPU --vm-memory "$INPUT_MEMORY"
 fi
 
 # Make some info available to the GitHub workflow.
 flyctl status --app "$app" --json >status.json
 hostname=$(jq -r .Hostname status.json)
 appid=$(jq -r .ID status.json)
-
 echo "hostname=$hostname" >> $GITHUB_OUTPUT
 echo "url=https://$hostname" >> $GITHUB_OUTPUT
 echo "id=$appid" >> $GITHUB_OUTPUT
