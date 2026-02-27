58d264d272ce73041acc5a1178e6790c67c1b3aa
Daniel Graña <dangra@gmail.com>
2026-02-27 16:01:10 -0300

    chore: pin alpine image and switch entrypoint to ash

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,5 @@
-#!/bin/sh -l
+#!/bin/ash -l
+# shellcheck shell=dash
 
 set -e
 
@@ -19,7 +20,8 @@ EVENT_TYPE=$(jq -r .action /github/workflow/event.json)
 # Default the Fly app name to pr-{number}-{repo_owner}-{repo_name}
 app="${INPUT_NAME:-pr-$PR_NUMBER-$GITHUB_REPOSITORY_OWNER-$GITHUB_REPOSITORY_NAME}"
 # Change underscores to hyphens.
-app=$(printf '%s' "$app" | tr '_' '-')
+# shellcheck disable=SC3060
+app="${app//_/-}"
 region="${INPUT_REGION:-${FLY_REGION:-iad}}"
 org="${INPUT_ORG:-${FLY_ORG:-personal}}"
 image="$INPUT_IMAGE"
